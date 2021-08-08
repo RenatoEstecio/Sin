d9997e89dca685fe9d90936cd9840ac66cf79dc8
Renato Estecio <renato@directd.com.br>
2021-08-08 11:16:53 -0300

    Upload

---
+++ Sin/Sin/Sin/Sin.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Sin
+{
+    partial class Sin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sin));
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.opfArq = new System.Windows.Forms.OpenFileDialog();
+            this.ttbCorrigir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ttbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(12, 12);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(112, 23);
+            this.btnProcurar.TabIndex = 0;
+            this.btnProcurar.Text = "Procurar Legenda";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // opfArq
+            // 
+            this.opfArq.FileName = "Arquivos SRT";
+            // 
+            // ttbCorrigir
+            // 
+            this.ttbCorrigir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ttbCorrigir.Location = new System.Drawing.Point(137, 162);
+            this.ttbCorrigir.Name = "ttbCorrigir";
+            this.ttbCorrigir.Size = new System.Drawing.Size(43, 26);
+            this.ttbCorrigir.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite um Inteiro Positivo ou Negativo ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ttbName
+            // 
+            this.ttbName.Enabled = false;
+            this.ttbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ttbName.Location = new System.Drawing.Point(12, 41);
+            this.ttbName.Multiline = true;
+            this.ttbName.Name = "ttbName";
+            this.ttbName.Size = new System.Drawing.Size(314, 65);
+            this.ttbName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Para Corrigir:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "(Segundos)";
+            // 
+            // Sin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(338, 252);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ttbName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ttbCorrigir);
+            this.Controls.Add(this.btnProcurar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Sin";
+            this.Text = "Sin";
+            this.Load += new System.EventHandler(this.Sin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.OpenFileDialog opfArq;
+        private System.Windows.Forms.TextBox ttbCorrigir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ttbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Sin/Sin/Sin/Sin.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sin
+{
+    public partial class Sin : Form
+    {
+        string legenda;
+        string loc;
+        public Sin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            opfArq.Filter = "SRT files (*.srt)|*.srt|All files (*.*)|*.*";/*Restringe o usuario 
+                                                                           * pra não escolher arquivos nao compativeis*/
+
+            if (opfArq.ShowDialog() == DialogResult.OK)/*Se caso ele mandou abrir algum arquivo*/
+            {
+                try
+                {
+                    ligaSin();
+                    ttbName.Text = opfArq.FileName; /*Copia o nome do arquivo p/ textbox*/
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro! :(");
+                }              
+            }
+        }
+        void desligaSin()
+        {
+            ttbCorrigir.Visible = false;
+            ttbName.Visible = false;
+            button1.Visible = false;
+            label1.Visible = false;
+        }
+        void ligaSin()
+        {
+            ttbCorrigir.Visible = true;
+            ttbName.Visible = true;
+            button1.Visible = true;
+            label1.Visible = true;
+        }
+        private void Sin_Load(object sender, EventArgs e)
+        {
+            desligaSin();
+        }
+        string SegToTime(string aux)/*Agora transforma os segundos devolta em tempo(Time)*/
+        {
+            decimal segundos = Convert.ToDecimal(aux);
+
+            int hora = (int) (segundos / 60) / 60;
+            segundos -= (hora * 60) * 60;
+            int min = (int)(segundos / 60);
+            segundos -= (min * 60) ;
+            string h = hora+"";
+            string m = min + "";
+            if (h.Length < 2)
+                h = "0" + h;
+            if (m.Length < 2)
+                m = "0" + m;
+            return h + ":" + m + ":" + segundos;
+        }
+        string Ajusta(string aux, decimal ajuste)
+        {
+            string[] time = aux.Split(':');/*Convert tempo em segundos e soma o que o usuario pediu*/
+            ajuste = Convert.ToDecimal(time[0]) * 60 * 60 +
+                     Convert.ToDecimal(time[1]) * 60 +
+                     Convert.ToDecimal(time[2]) + ajuste;
+
+            return ajuste + "";
+        }
+        void gravar(string s, string legenda)
+        {
+            s = s.Replace("_CorrigidaBySin", "[Copy]").Replace(".srt", "_CorrigidaBySin.srt");
+            System.IO.StreamWriter file = new System.IO.StreamWriter(s);
+            /*Manda por na mesma pasta do arquivo original mas muda o nome do arquivo*/
+            file.Write(legenda);
+            
+            file.Close();
+            loc= s;
+        }
+        void executa()
+        {
+            string aux1, aux2;
+            string lin;/**/
+
+            decimal Ajuste = Convert.ToDecimal(ttbCorrigir.Text);/*Pega os segundos que o usuario digitou*/
+            decimal tempo = 0;
+            if ((opfArq.OpenFile()) != null)
+            {
+                System.IO.StreamReader sr = new
+                System.IO.StreamReader(opfArq.FileName);
+
+                while (!sr.EndOfStream)
+                {
+                    lin = sr.ReadLine();/*Começa ler linha por linha*/
+
+                    if (lin.Contains("-->"))/*Vê se essa linha é sobre marcação de tempo*/
+                    {
+                        try {
+                            aux1 = Ajusta(lin.Split(' ')[0], Ajuste);/*Aqui ele pega o tempo que começa a fala*/
+                            aux2 = Ajusta(lin.Split('>')[1], Ajuste);/*Aqui ele pega o fim da fala(Quando some da tela)*/
+
+                            legenda += SegToTime(aux1) + " --> " + SegToTime(aux2) + "\r\n";
+                        }
+                        catch(Exception si) { }
+                    }
+                    else
+                        legenda += lin + "\r\n";
+                }
+                sr.Close();
+                sr.Dispose();
+                string name = opfArq.FileName;
+                opfArq.Dispose();
+                opfArq.Reset();
+                gravar(name, legenda);
+                MessageBox.Show("Concluido!\nArquivo criado em "+loc);/*Molezinha... viu? ^^ */
+                desligaSin();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ttbCorrigir.Text.Length > 0)
+            {
+                try
+                {
+                    executa();
+                }
+                catch (Exception si)
+                { MessageBox.Show("Erro!"); }
+            }
+            else
+                MessageBox.Show("Digite a quantidade de segundos!");
+
+        }
+    }
+}
